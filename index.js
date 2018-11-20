0b19a7443094871561c7e2128f0ecb1a3453f39b
prmorais13 <prmorais_13@hotmail.com>
2018-11-20 15:31:27 -0300

    Configurações e primeiros passos

---
+++ index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import './index.scss';
+
+ReactDOM.render(
+  <div>
+    <h2>Serviços</h2>
+    <hr />
+    <button>Aula Teórica</button>
+    <h2>Alunos</h2>
+    <hr />
+    <button>Aula Teórica</button>
+  </div>,
+  document.getElementById('root')
+);
